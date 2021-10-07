bb63d3d36ff0bc17cd38731e6d98223fc650379c
Shawn Koski <skoski@artnet.com>
2021-10-06 23:07:27 -0400

    IconInput at hover state

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,50 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const IconInput = ({ label, icon, width = 250, size, ...delegated }) => {
+    return (
+        <Wrapper>
+            <VisuallyHidden>{label}</VisuallyHidden>
+            <IconWrapper style={{ '--size': 16 + 'px' }}>
+                <Icon id={icon} size={16} strokeWidth={1} />
+            </IconWrapper>
+            <TextInput type="text" {...delegated}></TextInput>
+        </Wrapper>
+    );
 };
 
+const Wrapper = styled.label`
+    display: block;
+    position: relative;
+    color: ${COLORS.gray700};
+
+    &:hover {
+        color: ${COLORS.black};
+    }
+`;
+
+const IconWrapper = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    margin: auto 0;
+    height: var(--size);
+`;
+
+const TextInput = styled.input`
+    /* Use rem so input height will scale with default font-size of browser */
+    height: ${24 / 16}rem;
+    border: none;
+    border-bottom: 1px solid ${COLORS.black};
+    padding-left: 24px;
+    color: inherit;
+    font-weight: 700;
+    outline-offset: 2px;
+
+    &::placeholder {
+        color: ${COLORS.gray500};
+        font-weight: 400;
+    }
+`;
 export default IconInput;
